bd22263f94289c514d5e8a3f4e59fe4a18fa69e0
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-11-21 11:00:00 +0100

    added comfirmation before deleting

--- src/components/Tableheader.jsx
+++ src/components/Tableheader.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Table, Button, TextInput, } from "flowbite-react";
+import { Table, Button, TextInput, Label, Modal } from "flowbite-react";
+
 import { useNavigate } from "react-router-dom";
 import { deleteTodos, filterTodo } from "../redux/reducer";
 
 import { AiOutlinePlus } from 'react-icons/ai'
+import { HiOutlineExclamationCircle } from 'react-icons/hi2'
 import { connect, useDispatch } from "react-redux";
 import EditingTaskModal from './EditingTaskModal'
 
@@ -21,6 +23,7 @@ const mapStateToProps = (state) => {
 
 const Tableheader = (props) => {
   const [editTaskModal, setEditTaskModal] = useState(false)
+  const [deleteTaskModal, setDeleteTaskModal] = useState(false)
   const [todoName, setTodoName] = useState('')
   const [taskId, setTaskId] = useState(0)
   const [taskDate, setTaskDate] = useState('')
@@ -50,15 +53,20 @@ const Tableheader = (props) => {
       setEditTaskModal(true)
     }
   }
-
-  const deleTaskBtn = (id) => {
+  function deleTaskBtn(id) {
     if (!userLogged) {
-      navigate('/add')
+      navigate('/login')
     } else {
-      dispatch(
-        deleteTodos(id)
-      )
+      setDeleteTaskModal(true)
+      setTaskId(id)
     }
+
+  }
+  const deleteTask = () => {
+    dispatch(
+      deleteTodos(taskId)
+    )
+    setDeleteTaskModal(false)
   }
 
   function filterByDate(evt) {
@@ -66,7 +74,7 @@ const Tableheader = (props) => {
     let value = evt.target.value
     let newData = Data.filter(itm => itm.date === value)
     setAllTodos(newData)
-    
+
   }
 
   return (
@@ -81,9 +89,43 @@ const Tableheader = (props) => {
         taskDetail={taskDetail}
       />
 
+      <Modal
+        show={deleteTaskModal}
+        size="md"
+        popup={true}
+        onClose={() => setDeleteTaskModal(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this product?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="failure"
+              onClick={deleteTask}
+              >
+                Yes, I'm sure
+              </Button>
+              <Button
+                color="gray"
+                onClick={() => setDeleteTaskModal(false)}
+              >
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
 
       <div className='flex justify-between items-center mb-5'>
-        <div>
+        <div className='text-left'>
+          <Label
+            htmlFor="date"
+            value="Filter By date"
+          />
           <TextInput
             id="date"
             type="date"
