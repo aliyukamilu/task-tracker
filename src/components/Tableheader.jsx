5ad7c32b5a00138a316840b877037cf9accd1c69
fk-faruk <faruktaiwo@gmail.com>
2022-11-21 20:58:32 +0100

    code clean up

--- src/components/Tableheader.jsx
+++ src/components/Tableheader.jsx
@@ -4,10 +4,10 @@ import { Table, Button, TextInput, Label, Modal } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 import { deleteTodos, filterTodo } from "../redux/reducer";
 
-import { AiOutlinePlus } from 'react-icons/ai'
-import { HiOutlineExclamationCircle } from 'react-icons/hi2'
+import { AiOutlinePlus } from "react-icons/ai";
+import { HiOutlineExclamationCircle } from "react-icons/hi2";
 import { connect, useDispatch } from "react-redux";
-import EditingTaskModal from './EditingTaskModal'
+import EditingTaskModal from "./EditingTaskModal";
 
 const mapStateToProps = (state) => {
   return {
@@ -22,64 +22,58 @@ const mapStateToProps = (state) => {
 // };
 
 const Tableheader = (props) => {
-  const [editTaskModal, setEditTaskModal] = useState(false)
-  const [deleteTaskModal, setDeleteTaskModal] = useState(false)
-  const [todoName, setTodoName] = useState('')
-  const [taskId, setTaskId] = useState(0)
-  const [taskDate, setTaskDate] = useState('')
-  const [taskDetail, setTaskDetail] = useState('')
+  const [editTaskModal, setEditTaskModal] = useState(false);
+  const [deleteTaskModal, setDeleteTaskModal] = useState(false);
+  const [todoName, setTodoName] = useState("");
+  const [taskId, setTaskId] = useState(0);
+  const [taskDate, setTaskDate] = useState("");
+  const [taskDetail, setTaskDetail] = useState("");
 
-  const [allTodos, setAllTodos] = useState(props.todos)
+  const [allTodos, setAllTodos] = useState(props.todos);
 
-  const userLogged = sessionStorage.getItem('todo_user')
+  const userLogged = sessionStorage.getItem("todo_user");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
-
   const addTask = () => {
-    navigate('/add')
-  }
+    navigate("/add");
+  };
 
   const EditTaskBtn = (id, taskname, taskDate, taskDet) => {
     if (!userLogged) {
-      navigate('/add')
+      navigate("/add");
     } else {
-      setTodoName(taskname)
-      setTaskId(id)
-      setTaskDate(taskDate)
-      setTaskDetail(taskDet)
-      setEditTaskModal(true)
+      setTodoName(taskname);
+      setTaskId(id);
+      setTaskDate(taskDate);
+      setTaskDetail(taskDet);
+      setEditTaskModal(true);
     }
-  }
+  };
   function deleTaskBtn(id) {
     if (!userLogged) {
-      navigate('/login')
+      navigate("/login");
     } else {
-      setDeleteTaskModal(true)
-      setTaskId(id)
+      setDeleteTaskModal(true);
+      setTaskId(id);
     }
-
   }
   const deleteTask = () => {
-    dispatch(
-      deleteTodos(taskId)
-    )
-    setDeleteTaskModal(false)
-  }
+    dispatch(deleteTodos(taskId));
+    setDeleteTaskModal(false);
+  };
 
   function filterByDate(evt) {
-    let Data = props.todos
-    let value = evt.target.value
+    let Data = props.todos;
+    let value = evt.target.value;
     // let newData = Data.filter(itm => itm.date === value)
     // setAllTodos(newData)
-    dispatch(filterTodo(value))
+    dispatch(filterTodo(value));
   }
 
   return (
     <div className="allcontain pt-[120px]">
-
       <EditingTaskModal
         editTaskModal={editTaskModal}
         setEditTaskModal={setEditTaskModal}
@@ -100,19 +94,13 @@ const Tableheader = (props) => {
           <div className="text-center">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this product?
+              Are you sure you want to delete this Task
             </h3>
             <div className="flex justify-center gap-4">
-              <Button
-                color="failure"
-                onClick={deleteTask}
-              >
+              <Button color="failure" onClick={deleteTask}>
                 Yes, I'm sure
               </Button>
-              <Button
-                color="gray"
-                onClick={() => setDeleteTaskModal(false)}
-              >
+              <Button color="gray" onClick={() => setDeleteTaskModal(false)}>
                 No, cancel
               </Button>
             </div>
@@ -120,26 +108,28 @@ const Tableheader = (props) => {
         </Modal.Body>
       </Modal>
 
-      <div className='flex justify-between items-center mb-5'>
-        <div className='text-left'>
-          <Label
-            htmlFor="date"
-            value="Filter By date"
-          />
-          <TextInput
-            id="date"
-            type="date"
-            onChange={filterByDate}
-          />
+      <div className="flex justify-between items-center mb-5">
+        <div className="text-left">
+          <Label htmlFor="date" value="Filter By date" />
+          <TextInput id="date" type="date" onChange={filterByDate} />
         </div>
-        <div className=''>
-          <Button onClick={addTask}><AiOutlinePlus className='mr-3' size={20} /> Add Task</Button>
+        <div className="">
+          <Button onClick={addTask}>
+            <AiOutlinePlus className="mr-3" size={20} /> Add Task
+          </Button>
         </div>
       </div>
-      <div className='overflow-x-auto md:w-[70vw] w-[90vw]'>
-        <Table className='table-auto overflow-scroll w-full'>
+      <div className="overflow-x-auto md:w-[70vw] w-[90vw]">
+        <Table className="table-auto overflow-scroll w-full">
           <Table.Head>
-            {['Task', 'Date Created', 'Time', 'Details', 'Status', 'Action'].map((ittem, i) => (
+            {[
+              "Task",
+              "Date Created",
+              "Time",
+              "Details",
+              "Status",
+              "Action",
+            ].map((ittem, i) => (
               <Table.HeadCell key={i}>{ittem}</Table.HeadCell>
             ))}
           </Table.Head>
@@ -147,7 +137,10 @@ const Tableheader = (props) => {
             {props.todos.length > 0 &&
               props.todos.map((item, i) => {
                 return (
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={item.id}>
+                  <Table.Row
+                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                    key={item.id}
+                  >
                     <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                       {item.item}
                     </Table.Cell>
@@ -156,20 +149,35 @@ const Tableheader = (props) => {
                     <Table.Cell>{item.details}</Table.Cell>
                     <Table.Cell>{item.status}</Table.Cell>
                     <Table.Cell className="flex space-x-2">
-                      <Button size="xs" onClick={() => EditTaskBtn(item.id, item.item, item.date, item.details)}>Edit</Button>
-                      <Button size="xs" color='failure' onClick={() => deleTaskBtn(item.id)}>Delete</Button>
+                      <Button
+                        size="xs"
+                        onClick={() =>
+                          EditTaskBtn(
+                            item.id,
+                            item.item,
+                            item.date,
+                            item.details
+                          )
+                        }
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        size="xs"
+                        color="failure"
+                        onClick={() => deleTaskBtn(item.id)}
+                      >
+                        Delete
+                      </Button>
                     </Table.Cell>
                   </Table.Row>
-                )
+                );
               })}
-
-
           </Table.Body>
         </Table>
       </div>
     </div>
   );
 };
-
 
 export default connect(mapStateToProps)(Tableheader);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Button, TextInput, Label } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,43 +6,43 @@ const Login = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: "",
-    password: ""
-  })
+    password: "",
+  });
 
   function handleChange(evt) {
     const value = evt.target.value;
-    setFormData((prevState) => ({ ...prevState, [evt.target.id]: value }))
+    setFormData((prevState) => ({ ...prevState, [evt.target.id]: value }));
   }
 
+  const back = () => {
+    navigate("/");
+  };
   const logMeIn = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (formData.username == "" || formData.password == "") {
-      alert('Fill in the blanks')
+      alert("Fill in the blanks");
     } else if (formData.username === "faruk" && formData.password === "12345") {
       sessionStorage.setItem("todo_user", formData.username);
-      navigate('/')
+      navigate("/");
     } else {
-      alert('put in the correct credentials !!')
+      alert("put in the correct credentials !!");
     }
-
-
-  }
+  };
 
   return (
-    <div className='d-flex w-full'>
-      <div className='shadow-2xl rounded-md p-5 md:w-[500px] w-[380px]'>
+    <div className="d-flex w-full">
+      <Button onClick={back}>Back</Button>
+      <div className="shadow-2xl rounded-md p-5 md:w-[500px] w-[380px]">
         <form className="flex flex-col gap-4">
-          <h1 className='text-2xl font-bold'>Login</h1>
+          <h1 className="text-2xl font-bold">Login</h1>
           <div>
             <div className="mb-2 block text-left">
-              <Label
-                htmlFor="username"
-                value="Your username"
-              />
+              <Label htmlFor="username" value="Your username" />
             </div>
             <TextInput
               id="username"
+              placeholder="username"
               type="text"
               onChange={handleChange}
               defaultValue={formData.twitter}
@@ -52,27 +52,25 @@ const Login = () => {
           </div>
           <div>
             <div className="mb-2 block text-left">
-              <Label
-                htmlFor="password"
-                value="Your password"
-              />
+              <Label htmlFor="password" value="Your password" />
             </div>
             <TextInput
               id="password"
               type="password"
+              placeholder="password"
               onChange={handleChange}
               defaultValue={formData.twitter}
               required={true}
               shadow={true}
             />
           </div>
-          <Button type="submit" onClick={logMeIn} className='mt-10 w-full'>
+          <Button type="submit" onClick={logMeIn} className="mt-10 w-full">
             Login
           </Button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
